dda93687f0feff1c2973d4668b2e6360b727a912
Kathe2024 <caaldana03@tecnologicosudamericano.edu.ec>
2024-06-07 00:27:38 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+
+<?php
+$titulo = "Bienvenidos al enlace externo de php";
+?>
+<!DOCTYPE html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $titulo; ?></title>
+</head>
+<body>
+<h1>Bienvenidos al enlace externo de php</h1>
+<a href="http://www.php.net/docs.php/" 
+target="_blank"
+title="Enlace externo"
+class="class="miboton">Documentación oficial de PHP</a>
+    
+</body>
+</footer>
+<h1>Examen BIMESTRAL</h1>
+</html>
